b70df16206715a04674c64c9d0f7b79252fdc980
Emman <eman.darca86@gmail.com>
2024-05-15 19:04:53 +0800

    .

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from application import init_app
 app: Flask = init_app() # type: ignore
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=4000, debug=False)
+    app.run(port=4000, debug=False)
