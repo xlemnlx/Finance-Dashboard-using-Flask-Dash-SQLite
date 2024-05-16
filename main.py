30f6915bdc55ffbc6d44aed93ddd69346731a1a7
Emman <eman.darca86@gmail.com>
2024-05-16 14:28:46 +0800

    .

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from application import init_app
 
-app: Flask = init_app() # type: ignore 
+app: Flask = init_app() # type: ignore
 
 if __name__ == "__main__":
     app.run(debug=False)
